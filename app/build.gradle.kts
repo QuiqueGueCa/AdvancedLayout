659906794342580aec55538dc2312cdbaac03d8d
QuiqueGueCa <enrique.guevara@vasscompany.com>
2024-01-30 09:20:14 +0100

    Title and two paragraphs added

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.advancedlayout"
-    compileSdk = 33
+    compileSdk = 34
 
     defaultConfig {
         applicationId = "com.example.advancedlayout"
